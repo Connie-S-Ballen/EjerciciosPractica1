ad927d75c5aa9d3a494d0f2b0e68b720eb2e2db5
Connie-S-Ballen <nicolas.ballen2006@gmail.com>
2025-09-29 19:36:43 -0500

    se agregó funcion captura numero

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -4,13 +4,29 @@
 
 using namespace std;
 
+int CapturaNumero();
+
 int main()
 {
+
    vector<string> msg {"Hola", "C++", "from", "VS Code", "y la extension C++!"};
+   int numero_capturado;
 
    for (const string& word : msg)
    {
       cout << word << " ";
    }
    cout << endl;
+
+   numero_capturado = CapturaNumero();
+   
+   cout << "Numero: " << numero_capturado;
+}
+
+int CapturaNumero()
+{
+   int numero;
+   cout << "Ingrese un numero: ";
+   cin >> numero;
+   return numero;
 }
