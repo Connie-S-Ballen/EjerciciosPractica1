a00a6de590f316e32210df8cd9531c9102a75c5d
Connie-S-Ballen <nicolas.ballen2006@gmail.com>
2025-09-29 19:58:56 -0500

    Se agrega ejemplo para referenciar una variable como entrada de una función

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -4,13 +4,20 @@
 
 using namespace std;
 
-int CapturaNumero();
+// Prototipo de la funcion
+int CapturaNumero(int base);
+// Alternativa con referencia a la variable
+// Hacer referencia es cambiar el valor de la variable original
+//int CapturaNumero(int& base);
 
+// Funcion principal
 int main()
 {
 
    vector<string> msg {"Hola", "C++", "from", "VS Code", "y la extension C++!"};
    int numero_capturado;
+   
+   int base = 100;
 
    for (const string& word : msg)
    {
@@ -18,15 +25,21 @@ int main()
    }
    cout << endl;
 
-   numero_capturado = CapturaNumero();
+   numero_capturado = CapturaNumero(base);
    
-   cout << "Numero: " << numero_capturado;
+   cout << "Numero: " << numero_capturado << endl; 
+   cout << "Base: " << base << endl;
 }
 
-int CapturaNumero()
+// Definicion de la funcion
+// Alternativa con referencia a la variable
+// Hacer referencia es cambiar el valor de la variable original
+//int CapturaNumero(int& base)
+int CapturaNumero(int base)
 {
    int numero;
    cout << "Ingrese un numero: ";
    cin >> numero;
-   return numero;
+   base = base  + numero;
+   return base;
 }
