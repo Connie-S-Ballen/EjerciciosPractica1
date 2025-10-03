13b29717d2b6b29939864ed7b805a7b2bd1fac77
Connie-S-Ballen <nicolas.ballen2006@gmail.com>
2025-10-02 19:37:20 -0500

    ejercicio #1

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -5,29 +5,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Recorremos todos los pares posibles
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                // Si la suma de estos dos números es igual al target
-                if (nums[i] + nums[j] == target) {
-                    // Devolvemos los índices encontrados
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int* p1 = &nums[i];
+
+            for (int j = i + 1; j < n; j++) {
+                int* p2 = &nums[j];
+
+                if (*p1 + *p2 == target) {
                     return {i, j};
                 }
             }
         }
+        return {};
     }
 };
 
 int main() {
-    Solution solucion;
-
-    // Ejemplo de uso
-    vector<int> numeros = {2, 7, 11, 15};
-    int objetivo = 9;
-
-    vector<int> resultado = solucion.twoSum(numeros, objetivo);
-
-    cout << "Indices: " << resultado[0] << " y " << resultado[1] << endl;
-
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> resultado = sol.twoSum(nums, target);
+    cout << "Los indices son: " << resultado[0] << " y " << resultado[1] << endl;
     return 0;
 }
